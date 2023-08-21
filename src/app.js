7b75e8b9065f950f12c939718bdf23edc69b5cbe
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-08-21 14:47:25 +0000

    enhanced the suitgenerator/numbergenerator func

--- src/app.js
+++ src/app.js
@@ -5,23 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log(randomNumberGenerator(cardNumber, 13));
-  console.log(randomNumberGenerator(cardSuit, 4));
-};
-
-window.randomNumberGenerator = function randomNumberGenerator(anArray, number) {
-  let RandomNumber = Math.floor(Math.random() * number);
-  return anArray[RandomNumber];
-};
-
-  window.suitGenerator = function suitGenerator() {
-  let randSuit = randomNumberGenerator(cardSuit, 4);
-  let getSuitText = document.getElementById("#upperSuitText");
-  let suitText = document.createTextNode(randSuit);
-  getSuitText.appendChild(suitText);
-
-
 let cardNumber = [
   "A",
   "2",
@@ -39,3 +22,34 @@ let cardNumber = [
 ];
 
 let cardSuit = ["♦", "♥", "♠", "♣"];
+
+window.onload = function() {};
+
+window.randomNumberGenerator = function randomNumberGenerator(anArray, number) {
+  let RandomNumber = Math.floor(Math.random() * number);
+  return anArray[RandomNumber];
+};
+
+window.suitGenerator = function suitGenerator() {
+  let randSuit = randomNumberGenerator(cardSuit, 4);
+  const suitPara = document.createElement("p");
+  const suitText = document.createTextNode(randSuit);
+  suitPara.appendChild(suitText);
+  suitPara.classList.add("suitFont");
+  document.getElementById("upperSuit").appendChild(suitPara);
+  document.getElementById("lowerSuit").appendChild(suitPara);
+};
+
+window.numberGenerator = function numberGenerator() {
+  let randNumber = randomNumberGenerator(cardNumber, 13);
+  const numberPara = document.createElement("p");
+  const numberText = document.createTextNode(randNumber);
+  numberPara.appendChild(numberText);
+  numberPara.classList.add("cardNumberText");
+  document.getElementById("cardNumber").appendChild(numberPara);
+};
+
+window.turnRed = function turnRed(aTag) {
+  document.getElementsByClassName(aTag);
+  aTag.classList.add("red");
+};
