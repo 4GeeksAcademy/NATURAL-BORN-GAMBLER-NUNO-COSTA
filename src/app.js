47f91afc70f139037db6bae8e0c67d9171b02250
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-08-21 11:39:32 +0000

    added random func/suit generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(randomNumberGenerator(cardNumber, 13));
+  console.log(randomNumberGenerator(cardSuit, 4));
 };
+
+window.randomNumberGenerator = function randomNumberGenerator(anArray, number) {
+  let RandomNumber = Math.floor(Math.random() * number);
+  return anArray[RandomNumber];
+};
+
+  window.suitGenerator = function suitGenerator() {
+  let randSuit = randomNumberGenerator(cardSuit, 4);
+  let getSuitText = document.getElementById("#upperSuitText");
+  let suitText = document.createTextNode(randSuit);
+  getSuitText.appendChild(suitText);
+
+
+let cardNumber = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let cardSuit = ["♦", "♥", "♠", "♣"];
